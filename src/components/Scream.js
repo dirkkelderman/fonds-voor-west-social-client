bd626c94771219a9da635df3502cb210d289b103
Dirk Kelderman <dirkkelderman@gmail.com>
2021-04-17 21:52:40 +0200

    login page complete, login page styled, redirects to home page

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 // MUI Stuff
 import Card from "@material-ui/core/Card";
@@ -25,6 +27,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+      dayjs.extend(relativeTime)
     const {
       classes,
       scream: {
@@ -41,7 +44,7 @@ class Scream extends Component {
     return (
       <Card className={classes.card}>
         <CardMedia image={userImage} title="Profile Image" className={classes.image}/>
-        <CardContent class={classes.content}>
+        <CardContent className={classes.content}>
           <Typography 
             variant="h5" 
             component={Link} 
@@ -51,7 +54,7 @@ class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt.toLocaleString()}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
--- src/pages/home.js
+++ src/pages/home.js
@@ -11,7 +11,6 @@ export class home extends Component {
     componentDidMount(){
         axios.get('/screams')
             .then(res => {
-                console.log(res.data)
                 this.setState({
                     screams: res.data,
                 })
@@ -21,7 +20,7 @@ export class home extends Component {
 
     render() {
         let recentScreamsMarkup = this.state.screams ? (
-            this.state.screams.map(scream => <Scream scream={scream} />)
+            this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
         ) : <p>Loading...</p>
 
         return (
