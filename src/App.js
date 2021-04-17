b61a5b2acf35c23c50cf07848d896dd7ed81f358
Dirk Kelderman <dirkkelderman@gmail.com>
2021-04-17 20:57:09 +0200

    created scream cards, api request and styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //Componenets
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 // Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
           <div className="container">
             <Switch>
-              <Route exact path='/' component={home} />
-              <Route exact path='/login' component={login} />
-              <Route exact path='/signup' component={signup} />
-            </Switch> 
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
           </div>
-      </Router>
-    </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
