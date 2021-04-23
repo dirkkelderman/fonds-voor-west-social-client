5f7515b4340cc0b5cd703038be8bcd91ed35fad1
Dirk Kelderman <dirkkelderman@gmail.com>
2021-04-23 11:35:28 +0200

    Changed navbar, replaced buttons for a component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import EditDetails from './EditDetails'
+import MyButton from "../util/MyButton";
 
 //MUI stuff
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -10,8 +11,6 @@ import Button from "@material-ui/core/Button";
 import MuiLink from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-import IconButton from '@material-ui/core/IconButton'
-import ToolTip from '@material-ui/core/ToolTip'
 
 //Icons
 import LocationOn from "@material-ui/icons/LocationOn";
@@ -66,11 +65,9 @@ class Profile extends Component {
                 hidden="hidden"
                 onChange={this.handleImageChange}
               />
-                <ToolTip title="Edit profile picture" placement="top" >
-                    <IconButton onClick={this.handleEditPicture} className='button'>
-                        <EditIcon color="primary"/>
-                    </IconButton>
-                </ToolTip>
+                <MyButton tip="Edit profile picture" onClick={this.handleEditPicture} btnClassName="button">
+                    <EditIcon color="primary"/>
+                </MyButton>
             </div>
             <hr />
             <div className="profile-details">
@@ -104,11 +101,9 @@ class Profile extends Component {
               <CalendarToday color="primary" />{" "}
               <span>Joined {dayjs(createdAt).format("MMM YYYY")} </span>
             </div>
-            <ToolTip title="logout" placement='top'>
-                <IconButton onClick={this.handleLogout} >
-                    <KeyboardReturn color="primary" />
-                </IconButton>
-            </ToolTip>
+            <MyButton tip="logout" onClick={this.handleLogout} >
+                    <KeyboardReturn color="primary"/>
+                </MyButton>
             <EditDetails/>
           </div>
         </Paper>
---
+++ src/util/MyButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import ToolTip from '@material-ui/core/ToolTip'
+import IconButton from '@material-ui/core/IconButton'
+
+export default ({ children, onClick, tip, btnClassName, tipClassName}) => (
+    <ToolTip title={tip} className={tipClassName} placement="top">
+        <IconButton onClick={onClick} className={btnClassName}>
+            {children}
+        </IconButton>
+    </ToolTip>
+)
