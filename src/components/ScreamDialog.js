d0e191489046952cc3465bd5826eac8ee18cdd8f
Dirk Kelderman <dirkkelderman@gmail.com>
2021-05-03 14:10:26 +0200

    added a dialog for posting and styled it

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -4,11 +4,11 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import MyButton from '../util/MyButton'
 import dayjs from 'dayjs'
 import { Link } from 'react-router-dom'
+import LikeButton from "./LikeButton";
 
 // MUI stuff
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
-import DialogTitle from '@material-ui/core/DialogTitle'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
@@ -16,6 +16,7 @@ import Typography from '@material-ui/core/Typography'
 //Icons
 import CloseIcon  from '@material-ui/icons/Close'
 import UnfoldMore  from '@material-ui/icons/UnfoldMore'
+import ChatIcon from '@material-ui/icons/Chat'
 
 //Redux
 import { connect } from 'react-redux'
@@ -70,7 +71,9 @@ class ScreamDialog extends Component {
         const { classes, scream: { screamId, body, createdAt, likeCount, commentCount, userImage, userHandle }, UI: { loading } } = this.props
         
         const dialogMarkup = loading ? (
-            <CircularProgress size={200}/>
+            <div className={classes.spinnerDiv}>
+                <CircularProgress size={200} thickness={2} />
+            </div>
         ) : (
             <Grid container spacing={16}>
                 <Grid item sm={5}>
@@ -93,6 +96,12 @@ class ScreamDialog extends Component {
                         <Typography variant="body1">
                             {body}
                         </Typography>
+                        <LikeButton screamId={screamId} />
+                        <span>{likeCount} Likes</span>
+                        <MyButton tip="comments">
+                            <ChatIcon color="primary" />
+                        </MyButton>
+                        <span>{commentCount} comments</span>
                 </Grid>
             </Grid>
         )
